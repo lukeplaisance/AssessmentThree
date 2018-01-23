3edaefb0f23329fdfd907bdb27686d138cdac544
lukeplaisance <luke.plaisance17@gmail.com>
2018-01-23 17:33:05 -0600

    1/23/18
    changed around and added more text

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -11,16 +11,18 @@ int main()
 	LinkListType<int> *list = new LinkListType<int>();
 	while(outPut != 4)
 	{
+		cout << "Press 1, 2, 3, or 4 to choose what you want to do" << endl;
+		cout << endl;
 		if (outPut > 4) {
 			cout << "Invalid Choice :(" << endl;
 			system("pause");
 			continue;
 		}
-		cout << "1.Insert , 2.Delete, 3.Display, 4.Exit" << endl;
+		cout << "1.Insert Node || 2.Delete Node || 3.Display List || 4.Exit" << endl;
 		cout << endl;
-		cout << " Enter a choice \n";
+		cout << " Choose one of the four choices \n";
 		cin >> outPut;
-		cout << " Enter node of your choice" << endl;
+		cout << " Enter any number for the node" << endl;
 		cin >> value;
 		cout << endl;
 		system("cls");
@@ -38,10 +40,9 @@ int main()
 		case 3:
 			list->Print();
 			cout << endl;
-			break;
+				break;
 		case 4:
 			return 0;
-			break;
 		default:
 			cout << "Invalid" << endl;
 		}
